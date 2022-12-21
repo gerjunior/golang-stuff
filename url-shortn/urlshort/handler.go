7a27c2e1e5475b37d3708dc4cfb72ec486c31a41
Geraldo Jr <geraldojuni@hotmail.com>
2022-12-21 20:37:15 -0300

    refac: urlshort

--- url-shortn/urlshort/handler.go
+++ url-shortn/urlshort/handler.go
@@ -8,43 +8,48 @@ import (
 
 func MapHandler(pathToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		redirect(w, r, pathToUrls, fallback)
+		path := r.URL.Path
+
+		if url, found := pathToUrls[path]; found {
+			http.Redirect(w, r, url, http.StatusSeeOther)
+			return
+		}
+
+		fallback.ServeHTTP(w, r)
 	}
 }
 
-func redirect(w http.ResponseWriter, r *http.Request, pathToUrls map[string]string, fallback http.Handler) {
-	path := r.URL.Path
-	url := pathToUrls[path]
-
-	if url != "" {
-		http.Redirect(w, r, url, http.StatusSeeOther)
-		return
-	}
-
-	fallback.ServeHTTP(w, r)
-}
-
-type URLs struct {
+type pathUrl struct {
 	Path string
 	Url  string
 }
 
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	parsed := []URLs{}
-	err := yaml.Unmarshal(yml, &parsed)
+	parsedYaml, err := parseYaml(yml)
+	if err != nil {
+		return nil, err
+	}
+
+	pathToUrls := buildMap(parsedYaml)
+	return MapHandler(pathToUrls, fallback), nil
+}
+
+func parseYaml(ymlBytes []byte) ([]pathUrl, error) {
+	parsed := []pathUrl{}
+	err := yaml.Unmarshal(ymlBytes, &parsed)
 
 	if err != nil {
 		return nil, err
 	}
 
-	pathToUrls := make(map[string]string, 0)
-	for _, obj := range parsed {
+	return parsed, nil
+}
+
+func buildMap(data []pathUrl) map[string]string {
+	pathToUrls := map[string]string{}
+	for _, obj := range data {
 		pathToUrls[obj.Path] = obj.Url
 	}
 
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		redirect(w, r, pathToUrls, fallback)
-	}
-
-	return handler, nil
+	return pathToUrls
 }
