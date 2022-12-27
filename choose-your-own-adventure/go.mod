64eda389c8939499ab95fc41434b25d1867c80d8
Geraldo Jr <geraldojuni@hotmail.com>
2022-12-27 16:37:52 -0300

    feat: create an http handler for the adventure

---
+++ choose-your-own-adventure/go.mod
@@ -0,0 +1,3 @@
+module github.com/gerjunior/golang-stuff/choose-your-own-adventure
+
+go 1.18
--- choose-your-own-adventure/main.go
+++ choose-your-own-adventure/main.go
@@ -1,60 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-	"strconv"
+	"html/template"
+	"net/http"
+	"strings"
 
 	"github.com/gerjunior/golang-stuff/choose-your-own-adventure/story"
 )
 
-func read(book story.Book) {
-	arc := book["intro"]
-
-	for {
-		fmt.Printf("%s\n\n\n", arc.Title)
-
-		for _, paragraph := range arc.Story {
-			fmt.Printf("%s\n\n", paragraph)
-		}
-
-		if len(arc.Options) == 0 {
-			fmt.Println("The End")
-			break
-		}
-
-		fmt.Println("Choose an option:")
-
-		for idx, option := range arc.Options {
-			fmt.Printf("#%d) %s\n", idx+1, option.Text)
-		}
-
-		answerCh := make(chan int)
-
-		go func() {
-			for {
-				var input string
-				fmt.Scanf("%s", &input)
-				parsed, err := strconv.Atoi(input)
-				if err != nil {
-					continue
-				}
-
-				if parsed > len(arc.Options) {
-					continue
-				}
-
-				answerCh <- parsed
-				break
-			}
-		}()
-
-		select {
-		case answer := <-answerCh:
-			chosenArc := arc.Options[answer-1]
-			arc = book[chosenArc.Arc]
-		}
-	}
+func returnError(w http.ResponseWriter, res map[string]interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
 }
 
 func main() {
@@ -66,5 +25,26 @@ func main() {
 		panic(err)
 	}
 
-	read(book)
+	tmpl, err := template.New("Template.html").ParseFiles("Template.html")
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		arc := strings.Split(r.URL.Path, "/")[1]
+
+		story := book[arc]
+
+		err = tmpl.Execute(w, story)
+		if err != nil {
+			fmt.Println(err)
+			returnError(w, map[string]interface{}{
+				"error": "Unable to execute template",
+			})
+		}
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
